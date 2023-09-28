af59e04b557bf03c0fd7f76ba3e1a464465264bc
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-09-29 00:48:42 +0400

    task2 added

--- arrayTask/Program.cs
+++ arrayTask/Program.cs
@@ -2,11 +2,27 @@
 
 //Ədədlərdən ibarət arrayın elementleri cemini tapan alqoritm
 
-int[] arr = { 1, 3, 4, 5, 3, 2, 3, 4, 3 };
-int sum = 0;
+//int[] arr = { 1, 3, 4, 5, 3, 2, 3, 4, 3 };
+//int sum = 0;
 
-for (int i = 0; i < arr.Length; i++)
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum += arr[i];
+//}
+//Console.WriteLine($"Sum:{sum}");
+
+
+
+
+
+
+//Task2 
+
+//Sozlerden ibaret arrayde uzunlugu 4-den boyuk olan sozleri ekrana çap edin
+
+string[] arr = { "salam", "sagol", "yox" };
+
+for (int i = 0;i< arr.Length; i++)
 {
-    sum += arr[i];
+    if (arr[i].Length>4) Console.WriteLine(arr[i]);
 }
-Console.WriteLine($"Sum:{sum}");
