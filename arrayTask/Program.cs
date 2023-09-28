155e1f9c931db46560f31103d6fe34d74b06346c
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-09-29 00:44:37 +0400

    Add project files.

---
+++ arrayTask/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task1
+
+//Ədədlərdən ibarət arrayın elementleri cemini tapan alqoritm
+
+int[] arr = { 1, 3, 4, 5, 3, 2, 3, 4, 3 };
+int sum = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    sum += arr[i];
+}
+Console.WriteLine($"Sum:{sum}");
